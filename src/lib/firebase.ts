762b73f71ff9e90c0033162e4b2b67a2706ea465
Ángel Sande <angelsande@gmail.com>
2025-09-05 08:17:31 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy_placeholder",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:placeholder"
+  "projectId": "simpleauth-b0nan",
+  "appId": "1:413810157264:web:e80b6a31ef17d2742860d5",
+  "storageBucket": "simpleauth-b0nan.firebasestorage.app",
+  "apiKey": "AIzaSyA3baXl817mVfYJV7d5GOLuL9YSe3wSF3s",
+  "authDomain": "simpleauth-b0nan.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "413810157264"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
