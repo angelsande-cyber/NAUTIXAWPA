b948c825fc11ed2c00d468eac7bff3a4ae0ce750
Ángel Sande <angelsande@gmail.com>
2025-09-05 10:11:08 +0000

    Un mensaje que describa los cambios

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,14 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "simpleauth-b0nan",
-  "appId": "1:413810157264:web:e80b6a31ef17d2742860d5",
-  "storageBucket": "simpleauth-b0nan.firebasestorage.app",
-  "apiKey": "AIzaSyA3baXl817mVfYJV7d5GOLuL9YSe3wSF3s",
-  "authDomain": "simpleauth-b0nan.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "413810157264"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "simpleauth-b0nan",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:413810157264:web:e80b6a31ef17d2742860d5",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "simpleauth-b0nan.firebasestorage.app",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyA3baXl817mVfYJV7d5GOLuL9YSe3wSF3s",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "simpleauth-b0nan.firebaseapp.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "413810157264",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
