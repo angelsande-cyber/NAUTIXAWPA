9bd748504b7e184deb0d879f3a9a716cfd3c577e
Ángel Sande <angelsande@gmail.com>
2025-09-07 08:19:59 +0000

    aqui tienes el nombre y la ruta, ahora no puedes fallar

--- src/lib/data/clouds.ts
+++ src/lib/data/clouds.ts
@@ -1,14 +1,14 @@
 // Data for cloud types, using translation keys.
 
 export const CLOUD_DATA = [
-    { type: 'meteo.clouds.types.cirrus.name', altitude: 'meteo.clouds.types.cirrus.altitude', description: 'meteo.clouds.types.cirrus.description', imageUrl: '/images/clouds/cirrus.png', hint: 'cirrus wispy' },
-    { type: 'meteo.clouds.types.cirrocumulus.name', altitude: 'meteo.clouds.types.cirrocumulus.altitude', description: 'meteo.clouds.types.cirrocumulus.description', imageUrl: '/images/clouds/cirrocumulus.png', hint: 'cirrocumulus mackerel' },
-    { type: 'meteo.clouds.types.cirrostratus.name', altitude: 'meteo.clouds.types.cirrostratus.altitude', description: 'meteo.clouds.types.cirrostratus.description', imageUrl: '/images/clouds/cirrostratus.png', hint: 'cirrostratus halo' },
-    { type: 'meteo.clouds.types.altocumulus.name', altitude: 'meteo.clouds.types.altocumulus.altitude', description: 'meteo.clouds.types.altocumulus.description', imageUrl: '/images/clouds/altocumulus.png', hint: 'altocumulus midlevel' },
-    { type: 'meteo.clouds.types.altostratus.name', altitude: 'meteo.clouds.types.altostratus.altitude', description: 'meteo.clouds.types.altostratus.description', imageUrl: '/images/clouds/altostratus.png', hint: 'altostratus sheet' },
-    { type: 'meteo.clouds.types.nimbostratus.name', altitude: 'meteo.clouds.types.nimbostratus.altitude', description: 'meteo.clouds.types.nimbostratus.description', imageUrl: '/images/clouds/nimbostratus.png', hint: 'nimbostratus rain' },
-    { type: 'meteo.clouds.types.stratus.name', altitude: 'meteo.clouds.types.stratus.altitude', description: 'meteo.clouds.types.stratus.description', imageUrl: '/images/clouds/stratus.png', hint: 'stratus layer' },
-    { type: 'meteo.clouds.types.stratocumulus.name', altitude: 'meteo.clouds.types.stratocumulus.altitude', description: 'meteo.clouds.types.stratocumulus.description', imageUrl: '/images/clouds/stratocumulus.png', hint: 'stratocumulus low' },
-    { type: 'meteo.clouds.types.cumulus.name', altitude: 'meteo.clouds.types.cumulus.altitude', description: 'meteo.clouds.types.cumulus.description', imageUrl: '/images/clouds/cumulus.png', hint: 'cumulus fluffy' },
-    { type: 'meteo.clouds.types.cumulonimbus.name', altitude: 'meteo.clouds.types.cumulonimbus.altitude', description: 'meteo.clouds.types.cumulonimbus.description', imageUrl: '/images/clouds/cumulonimbus.png', hint: 'cumulonimbus storm' }
+    { type: 'meteo.clouds.types.cirrus.name', altitude: 'meteo.clouds.types.cirrus.altitude', description: 'meteo.clouds.types.cirrus.description', imageUrl: '/images/clouds/Cirrus.png', hint: 'cirrus wispy' },
+    { type: 'meteo.clouds.types.cirrocumulus.name', altitude: 'meteo.clouds.types.cirrocumulus.altitude', description: 'meteo.clouds.types.cirrocumulus.description', imageUrl: '/images/clouds/Cirrocumulus.png', hint: 'cirrocumulus mackerel' },
+    { type: 'meteo.clouds.types.cirrostratus.name', altitude: 'meteo.clouds.types.cirrostratus.altitude', description: 'meteo.clouds.types.cirrostratus.description', imageUrl: '/images/clouds/Cirrostratus.png', hint: 'cirrostratus halo' },
+    { type: 'meteo.clouds.types.altocumulus.name', altitude: 'meteo.clouds.types.altocumulus.altitude', description: 'meteo.clouds.types.altocumulus.description', imageUrl: '/images/clouds/Altocumulus.png', hint: 'altocumulus midlevel' },
+    { type: 'meteo.clouds.types.altostratus.name', altitude: 'meteo.clouds.types.altostratus.altitude', description: 'meteo.clouds.types.altostratus.description', imageUrl: '/images/clouds/Altostratus.png', hint: 'altostratus sheet' },
+    { type: 'meteo.clouds.types.nimbostratus.name', altitude: 'meteo.clouds.types.nimbostratus.altitude', description: 'meteo.clouds.types.nimbostratus.description', imageUrl: '/images/clouds/Nimbostratus.png', hint: 'nimbostratus rain' },
+    { type: 'meteo.clouds.types.stratus.name', altitude: 'meteo.clouds.types.stratus.altitude', description: 'meteo.clouds.types.stratus.description', imageUrl: '/images/clouds/Stratus.png', hint: 'stratus layer' },
+    { type: 'meteo.clouds.types.stratocumulus.name', altitude: 'meteo.clouds.types.stratocumulus.altitude', description: 'meteo.clouds.types.stratocumulus.description', imageUrl: '/images/clouds/Stratocumulus.png', hint: 'stratocumulus low' },
+    { type: 'meteo.clouds.types.cumulus.name', altitude: 'meteo.clouds.types.cumulus.altitude', description: 'meteo.clouds.types.cumulus.description', imageUrl: '/images/clouds/Cumulus.png', hint: 'cumulus fluffy' },
+    { type: 'meteo.clouds.types.cumulonimbus.name', altitude: 'meteo.clouds.types.cumulonimbus.altitude', description: 'meteo.clouds.types.cumulonimbus.description', imageUrl: '/images/clouds/CUMULONIMBUS.png', hint: 'cumulonimbus storm' }
 ];
