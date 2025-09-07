2949a133e5127ef0e63eee4e408c749eb86363db
Ángel Sande <angelsande@gmail.com>
2025-09-07 06:48:16 +0000

    no se traduce

---
+++ src/lib/data/colreg.ts
@@ -0,0 +1,124 @@
+// Data structure for the COLREG study guide.
+// Uses translation keys to be resolved by the component.
+
+export const COLREG_DATA = [
+    {
+        id: "part-A",
+        title: "colreg.partA.title",
+        sections: [
+            {
+                rules: [
+                    {
+                        id: "rule-1",
+                        title: "colreg.partA.rule1.title",
+                        points: [
+                            { text: "colreg.partA.rule1.point1" },
+                            { text: "colreg.partA.rule1.point2" },
+                            { text: "colreg.partA.rule1.point3" },
+                            { text: "colreg.partA.rule1.point4" },
+                            { text: "colreg.partA.rule1.point5" },
+                        ]
+                    },
+                    {
+                        id: "rule-2",
+                        title: "colreg.partA.rule2.title",
+                        points: [
+                            { text: "colreg.partA.rule2.point1" },
+                            { text: "colreg.partA.rule2.point2" },
+                            { text: "colreg.partA.rule2.point3" },
+                        ]
+                    },
+                    {
+                        id: "rule-3",
+                        title: "colreg.partA.rule3.title",
+                        points: [
+                            {
+                                text: "colreg.partA.rule3.point1",
+                                subpoints: [
+                                    "colreg.partA.rule3.subpoint1",
+                                    "colreg.partA.rule3.subpoint2",
+                                    "colreg.partA.rule3.subpoint3",
+                                    "colreg.partA.rule3.subpoint4",
+                                    "colreg.partA.rule3.subpoint5",
+                                    "colreg.partA.rule3.subpoint6",
+                                    "colreg.partA.rule3.subpoint7",
+                                    "colreg.partA.rule3.subpoint8",
+                                ]
+                            },
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: "part-B",
+        title: "colreg.partB.title",
+        sections: [
+            {
+                title: "colreg.partB.section1.title",
+                rules: [
+                    { id: "rule-5", title: "colreg.partB.section1.rule5.title", points: [{ text: "colreg.partB.section1.rule5.point1" }, { text: "colreg.partB.section1.rule5.point2" }, { text: "colreg.partB.section1.rule5.point3" }] },
+                    { id: "rule-6", title: "colreg.partB.section1.rule6.title", points: [{ text: "colreg.partB.section1.rule6.point1" }, { text: "colreg.partB.section1.rule6.point2" }] },
+                    { id: "rule-7", title: "colreg.partB.section1.rule7.title", points: [{ text: "colreg.partB.section1.rule7.point1" }, { text: "colreg.partB.section1.rule7.point2" }, { text: "colreg.partB.section1.rule7.point3" }] },
+                    { id: "rule-8", title: "colreg.partB.section1.rule8.title", points: [{ text: "colreg.partB.section1.rule8.point1" }, { text: "colreg.partB.section1.rule8.point2" }, { text: "colreg.partB.section1.rule8.point3" }, { text: "colreg.partB.section1.rule8.point4" }] },
+                    { id: "rule-9", title: "colreg.partB.section1.rule9.title", points: [{ text: "colreg.partB.section1.rule9.point1" }, { text: "colreg.partB.section1.rule9.point2" }, { text: "colreg.partB.section1.rule9.point3" }, { text: "colreg.partB.section1.rule9.point4" }] },
+                    { id: "rule-10", title: "colreg.partB.section1.rule10.title", points: [{ text: "colreg.partB.section1.rule10.point1" }, { text: "colreg.partB.section1.rule10.point2" }, { text: "colreg.partB.section1.rule10.point3" }, { text: "colreg.partB.section1.rule10.point4" }] },
+                ]
+            },
+            {
+                title: "colreg.partB.section2.title",
+                rules: [
+                    { id: "rule-12", title: "colreg.partB.section2.rule12.title", points: [{ text: "colreg.partB.section2.rule12.point1" }, { text: "colreg.partB.section2.rule12.point2" }, { text: "colreg.partB.section2.rule12.point3" }] },
+                    { id: "rule-13", title: "colreg.partB.section2.rule13.title", points: [{ text: "colreg.partB.section2.rule13.point1" }, { text: "colreg.partB.section2.rule13.point2" }] },
+                    { id: "rule-14", title: "colreg.partB.section2.rule14.title", points: [{ text: "colreg.partB.section2.rule14.point1" }] },
+                    { id: "rule-15", title: "colreg.partB.section2.rule15.title", points: [{ text: "colreg.partB.section2.rule15.point1" }] },
+                    { id: "rule-16", title: "colreg.partB.section2.rule16.title", points: [{ text: "colreg.partB.section2.rule16.point1" }] },
+                    { id: "rule-17", title: "colreg.partB.section2.rule17.title", points: [{ text: "colreg.partB.section2.rule17.point1" }] },
+                    { id: "rule-18", title: "colreg.partB.section2.rule18.title", points: [{ text: "colreg.partB.section2.rule18.point1" }] },
+                ]
+            },
+            {
+                title: "colreg.partB.section3.title",
+                rules: [
+                    { id: "rule-19", title: "colreg.partB.section3.rule19.title", points: [{ text: "colreg.partB.section3.rule19.point1" }, { text: "colreg.partB.section3.rule19.point2" }] },
+                ]
+            }
+        ]
+    },
+    {
+        id: "part-C",
+        title: "colreg.partC.title",
+        sections: [
+            {
+                rules: [
+                    { id: "rule-20", title: "colreg.partC.rule20.title", points: [{ text: "colreg.partC.rule20.point1" }] },
+                    { id: "rule-21-31", title: "colreg.partC.rule21-31.title", points: [{ text: "colreg.partC.rule21-31.point1" }] },
+                ]
+            }
+        ]
+    },
+    {
+        id: "part-D",
+        title: "colreg.partD.title",
+        sections: [
+            {
+                rules: [
+                    { id: "rule-32-37", title: "colreg.partD.rule32-37.title", points: [{ text: "colreg.partD.rule32-37.point1" }] },
+                ]
+            }
+        ]
+    },
+    {
+        id: "part-EF",
+        title: "colreg.partEF.title",
+        sections: [
+            {
+                rules: [
+                    { id: "rule-38", title: "colreg.partEF.rule38.title", points: [{ text: "colreg.partEF.rule38.point1" }] },
+                    { id: "rule-39-41", title: "colreg.partEF.rule39-41.title", points: [{ text: "colreg.partEF.rule39-41.point1" }] },
+                ]
+            }
+        ]
+    }
+];
