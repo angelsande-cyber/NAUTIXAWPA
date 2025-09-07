8f1a33760f17e9770bd0c56c373f9b6f7c684321
Ángel Sande <angelsande@gmail.com>
2025-09-07 08:20:56 +0000

    Cumulonimbus sigue sin verse

--- src/lib/data/clouds.ts
+++ src/lib/data/clouds.ts
@@ -3,7 +3,7 @@
 export const CLOUD_DATA = [
     { type: 'meteo.clouds.types.cirrus.name', altitude: 'meteo.clouds.types.cirrus.altitude', description: 'meteo.clouds.types.cirrus.description', imageUrl: '/images/clouds/Cirrus.png', hint: 'cirrus wispy' },
     { type: 'meteo.clouds.types.cirrocumulus.name', altitude: 'meteo.clouds.types.cirrocumulus.altitude', description: 'meteo.clouds.types.cirrocumulus.description', imageUrl: '/images/clouds/Cirrocumulus.png', hint: 'cirrocumulus mackerel' },
-    { type: 'meteo.clouds.types.cirrostratus.name', altitude: 'meteo.clouds.types.cirrostratus.altitude', description: 'meteo.clouds.types.cirrostratus.description', imageUrl: '/images/clouds/Cirrostratus.png', hint: 'cirrostratus halo' },
+    { type: 'meteo.clouds.types.cirrostratus.name', altitude: 'meteo.clouds.types.cirrostratus.altitude', description: 'meteo.clouds.types.cirrostratus.description', imageUrl: '/images/clouds/cirrostratus.png', hint: 'cirrostratus halo' },
     { type: 'meteo.clouds.types.altocumulus.name', altitude: 'meteo.clouds.types.altocumulus.altitude', description: 'meteo.clouds.types.altocumulus.description', imageUrl: '/images/clouds/Altocumulus.png', hint: 'altocumulus midlevel' },
     { type: 'meteo.clouds.types.altostratus.name', altitude: 'meteo.clouds.types.altostratus.altitude', description: 'meteo.clouds.types.altostratus.description', imageUrl: '/images/clouds/Altostratus.png', hint: 'altostratus sheet' },
     { type: 'meteo.clouds.types.nimbostratus.name', altitude: 'meteo.clouds.types.nimbostratus.altitude', description: 'meteo.clouds.types.nimbostratus.description', imageUrl: '/images/clouds/Nimbostratus.png', hint: 'nimbostratus rain' },
