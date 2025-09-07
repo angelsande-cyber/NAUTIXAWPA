54e8d3ecf2fad28addc358dc01ff92283544c263
Ángel Sande <angelsande@gmail.com>
2025-09-07 10:26:33 +0000

    CNMC no es necesario, añade teléfono del servicio radiomédico, y alguno

--- src/lib/data/directorio.ts
+++ src/lib/data/directorio.ts
@@ -14,27 +14,27 @@ export const DIRECTORY_DATA = [
         keywords: ["Policía", "Vigilancia", "Seguridad", "Denuncia"]
     },
     {
+        name: "Servicio Radiomédico Español",
+        phones: ["91 310 34 75"],
+        email: "cr-radiomedico@seg-social.es",
+        keywords: ["Médico", "Consulta", "Salud", "Accidente", "Radiomédico"]
+    },
+    {
         name: "Cruz Roja del Mar",
         phones: ["901 222 222"],
         email: null,
         keywords: ["Asistencia", "Socorrismo", "Primeros auxilios", "Playas"]
     },
     {
-        name: "Policía Nacional",
-        phones: ["091"],
-        email: null,
-        keywords: ["Policía", "Seguridad", "Denuncia"]
-    },
-    {
-        name: "CNMC (Comisión Nacional de los Mercados y la Competencia)",
-        phones: ["91 021 00 00"],
-        email: "info@cnmc.es",
-        keywords: ["Licencias", "Radio", "Telecomunicaciones", "Inspección"]
-    },
-    {
         name: "AEMET (Agencia Estatal de Meteorología)",
         phones: ["807 17 00 17", "91 581 99 99"],
         email: null,
         keywords: ["Tiempo", "Meteorología", "Previsión", "Avisos"]
-    }
+    },
+    {
+        name: "Instituto Nacional de Toxicología",
+        phones: ["91 562 04 20"],
+        email: null,
+        keywords: ["Veneno", "Intoxicación", "Químicos", "Toxicología"]
+    },
 ];
